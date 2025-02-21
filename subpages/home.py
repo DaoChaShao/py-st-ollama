1b5dec342e22620dc86560e9f960d549dace1142
DaoChaShao <27882272@qq.com>
2025-02-21 20:43:26 +1300

    chore: complete the instructions of using the application.

--- subpages/home.py
+++ subpages/home.py
@@ -6,10 +6,21 @@
 # @File     :   home.py
 # @Desc     :   
 
-from streamlit import title, divider, expander, caption, empty
+from streamlit import title, divider, expander, caption, empty, sidebar, markdown
 
 title("Call a Ollama Model Locally")
 divider()
+
+empty_message: empty = empty()
+
+with sidebar:
+    "[![Static Badge](https://img.shields.io/badge/GitHub-py--st--ollama%20in%20DaoChaShao-black?style=for-the-badge&logo=github)](https://github.com/DaoChaShao/py-st-ollama)"
+
 with expander("**TARGET**: Download the Ollama model and run it locally.", expanded=True):
     caption("1. Download the **INSTALLER** from [Ollama](https://ollama.com/).")
     caption("2. Download the **MODEL** you want to use.")
+    caption("3. Keep the **INSTALLER** running.")
+    caption("4. Clone the code from the **GitHub** to the local.")
+    caption("5. Use the command `streamlit run main.py` to use this application.")
+
+empty_message.info("You can call the Model at the **Models** page.")
