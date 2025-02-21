08b5ae0283b792bcf1091d369e99fb1bc65699f5
DaoChaShao <27882272@qq.com>
2025-02-21 20:44:46 +1300

    feat: add the Ollama model call functions.

---
+++ subpages/ollama.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/2/21 17:58
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   ollama.py
+# @Desc     :   
+
+from streamlit import (chat_input, empty, chat_message, write, sidebar,
+                       session_state, button, caption, rerun, divider,
+                       image)
+
+from utilis.tools import parameters, model_caller, Timer
+
+# Initialize the chat history
+if "messages" not in session_state:
+    session_state.messages = []
+
+empty_message: empty = empty()
+
+model, temperature, top_p = parameters()
+prompt: str = chat_input("Say something", max_chars=100)
+
+if model != "Select a Model":
+
+    # Display the chat history firstly
+    for msg in session_state.messages:
+        with chat_message(msg["role"]):
+            write(msg["content"])
+
+    if prompt:
+        # Add the user input to the chat history
+        session_state.messages.append({"role": "user", "content": prompt})
+        # Display the user input
+        with chat_message("user"):
+            write(prompt)
+
+        with Timer(2, description="Local Model Call") as timer:
+            with chat_message("assistant"):
+                response = model_caller(model, temperature, top_p, prompt)  # 生成 AI 回复
+                write(response)
+
+                # Add the AI response to the chat history
+                session_state.messages.append({"role": "assistant", "content": response})
+
+        empty_message.success(f"**{str(timer)}**")
+    else:
+        empty_message.error("Please enter a prompt to generate a response.")
+else:
+    empty_message.info("Please select a model from the sidebar.")
+
+with sidebar:
+    caption(f"**{int(len(session_state.messages) / 2)}** round messages in the chat history.")
+    if button("Clear History", type="primary", help="Click to clear the chat history."):
+        session_state.messages = []
+        empty_message.success("The history has been cleared.")
+        rerun()
