5991cb3d7da3cd53f4468e3cf44d837a7aeb57db
DaoChaShao <27882272@qq.com>
2025-02-21 22:42:23 +1300

    chore: add some documentation of the functions.

--- utilis/tools.py
+++ utilis/tools.py
@@ -12,20 +12,38 @@ from time import perf_counter
 
 
 def parameters() -> tuple[str, int, int]:
+    """ Set the hyperparameters for the Ollama model.
+
+    :return: the model name, temperature, and top P
+    """
     with sidebar:
-        header("Parameters")
+        param_temp = 0.0
+        param_top = 0.0
+        header("Hyperparameters")
+
         options: list = ["qwen2.5:14b", "phi4:latest", "llama3.1:latest"]
         model: str = selectbox(
             "Model", ["Select a Model"] + options, help="Select a model"
         )
-        if model in options:
-            caption(f"The model you selected is: **{model}**")
+        match model:
+            case "qwen2.5:14b":
+                caption(f"The model you selected is: **{model}**")
+                param_temp: float = 1.0
+                param_top: float = 1.0
+            case "phi4:latest":
+                caption(f"The model you selected is: **{model}**")
+                param_temp: float = 0.7
+                param_top: float = 0.8
+            case "llama3.1:latest":
+                caption(f"The model you selected is: **{model}**")
+                param_temp: float = 0.8
+                param_top: float = 0.9
 
-        temperature: int = slider("Temperature", 0.0, 2.0, 0.8, help="The randomness of the output.")
+        temperature: int = slider("Temperature", 0.0, 2.0, param_temp, help="The randomness of the output.")
         if temperature:
             caption(f"The temperature you selected is: **{temperature}**")
 
-        top_p: int = slider("Top P", 0.0, 1.0, 0.9, help="The probability of the output.")
+        top_p: int = slider("Top P", 0.0, 1.0, param_top, help="The probability of the output.")
         if top_p:
             caption(f"The top P you selected is: **{top_p}**")
 
@@ -57,6 +75,12 @@ def model_caller(model_name: str, temperature: float, top_p: int, prompt: str) -
 
 
 class Timer(object):
+    """ A simple timer class to measure the elapsed time.
+
+    :param precision: the number of decimal places to round the elapsed time
+    :param description: the description of the timer
+    """
+
     def __init__(self, precision: int = 5, description: str = None):
         self._precision = precision
         self._description = description
